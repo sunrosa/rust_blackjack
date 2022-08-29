124a926c067aecd05f394f0b2ad486dcd3327834
Ibis Sunrosa <drsunrosa@gmail.com>
2022-08-29 06:03:17 -0500

    Refactoring for split command

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod data;
 
 use std::{thread, time::Duration};
 
+use deckofcards::Card;
+
 fn main() {
     // Define game state
     let cfg = data::Configuration {
@@ -22,16 +24,17 @@ fn main() {
 
         // Define game state
         let mut deck = deckofcards::Deck::new();
-        let mut player_hand = deckofcards::Hand::new();
+        let mut player_hands = vec![deckofcards::Hand::new()];
         let mut dealer_hand = deckofcards::Hand::new();
         let mut result = data::GameResult::Unfinished;
-        let bet: u32;
+        let mut result_split: data::GameResult;
+        let mut bet: u32;
 
         // Initialize game
         deckofcards::Cards::shuffle(&mut deck);
-        deck.deal_to_hand(&mut player_hand, 1);
+        deck.deal_to_hand(&mut player_hands[0], 1);
         deck.deal_to_hand(&mut dealer_hand, 1);
-        deck.deal_to_hand(&mut player_hand, 1);
+        deck.deal_to_hand(&mut player_hands[0], 1);
         deck.deal_to_hand(&mut dealer_hand, 1);
 
         typeln(
@@ -73,14 +76,14 @@ fn main() {
 
         // Print initial game state to user
         typeln(&String::from("Your hand:"), &cfg);
-        type_hand(&player_hand, &cfg); // Print all cards in player hand
+        type_hand(&player_hands[0], &cfg); // Print all cards in player hand
         typeln(&String::from("Dealer hand:"), &cfg);
         type_hand(
             &deckofcards::Hand::from_cards(&dealer_hand.cards[0..1]),
             &cfg,
         ); // Print first card only in dealer's hand
 
-        if hand_value(&player_hand) == 21 && player_hand.cards.len() as u8 == 2 {
+        if hand_value(&player_hands[0]) == 21 && player_hands[0].cards.len() as u8 == 2 {
             result = data::GameResult::Blackjack;
         }
         // Command loop
@@ -99,10 +102,10 @@ fn main() {
 
                 match input.as_str() {
                     "hit" => {
-                        deck.deal_to_hand(&mut player_hand, 1);
-                        type_hand(&player_hand, &cfg);
+                        deck.deal_to_hand(&mut player_hands[0], 1);
+                        type_hand(&player_hands[0], &cfg);
 
-                        if hand_value(&player_hand) > 21 {
+                        if hand_value(&player_hands[0]) > 21 {
                             result = data::GameResult::Bust;
                             break;
                         }
@@ -117,18 +120,18 @@ fn main() {
 
                         type_hand(&dealer_hand, &cfg);
 
-                        let player_hand_value = hand_value(&player_hand);
+                        let player_hand_value = hand_value(&player_hands[0]);
                         let dealer_hand_value = hand_value(&dealer_hand);
                         if player_hand_value > 21 {
                             result = data::GameResult::Bust;
-                        } else if player_hand_value == 21 && player_hand.cards.len() == 2 {
+                        } else if player_hand_value == 21 && player_hands[0].cards.len() == 2 {
                             result = data::GameResult::Blackjack;
                         } else if dealer_hand_value > 21 && player_hand_value <= 21 {
                             result = data::GameResult::DealerBust;
                         } else if player_hand_value > dealer_hand_value && player_hand_value <= 21 {
                             result = data::GameResult::Win;
                         } else if player_hand_value == dealer_hand_value
-                            && !(player_hand_value == 21 && player_hand.cards.len() == 2)
+                            && !(player_hand_value == 21 && player_hands[0].cards.len() == 2)
                         {
                             result = data::GameResult::Push;
                         } else if player_hand_value < dealer_hand_value {
@@ -137,8 +140,46 @@ fn main() {
 
                         break;
                     }
+                    "double" => {
+                        // Exit if the player has already hit
+                        if player_hands[0].cards.len() != 2 {
+                            println!("Doubling down is only allowed before hitting.");
+                            continue;
+                        }
+
+                        // Double the bet
+                        stats.decrease_wallet(bet);
+                        stats.bet(bet);
+                        bet *= 2;
+
+                        // Deal one additional card
+                        deck.deal_to_hand(&mut player_hands[0], 1);
+                        type_hand(&player_hands[0], &cfg);
+
+                        // Bust automatically if over 21
+                        if hand_value(&player_hands[0]) > 21 {
+                            result = data::GameResult::Bust;
+                            break;
+                        }
+                    }
+                    /*"split" => {
+                        if player_hands.cards.len() != 2
+                            || card_value(&player_hands.cards.get(0).unwrap())
+                                != card_value(&player_hands.cards.get(1).unwrap())
+                        {
+                            println!("You may only split when you have two cards of equal rank.")
+                        }
+
+                        // Split the hands in two
+                        player_hands.remove(0);
+                        player_hand_split = player_hands.clone();
+
+                        // Deal one card to each hand
+                        deck.deal_to_hand(&mut player_hands, 1);
+                        deck.deal_to_hand(&mut player_hand_split, 1);
+                    }*/
                     "help" => {
-                        println!("hit: Receive an additional card for your hand.\nstand: Keep your current hand and advance the game.\nhelp: Print help.\nquit: Quit the program.")
+                        println!("hit: Receive an additional card for your hand.\nstand: Keep your current hand and advance the game.\ndouble: Double your bet and gain an additional card (can only be used before hitting for the first time).\nsplit: Split first two cards of same value into two separate hands, betting additionally for your second hand.\nhelp: Print help.\nquit: Quit the program.")
                     }
                     "quit" => quit(&cfg, &stats),
                     _ => println!("Invalid command! Type \"help\" for more information."),
@@ -148,21 +189,24 @@ fn main() {
 
         match result {
             data::GameResult::Win => {
-                typeln(&String::from("Win!"), &cfg);
+                typeln(&format!("Win! +({}{})", cfg.currency_prefix, bet), &cfg);
                 stats.increase_wallet(bet * 2);
                 stats.pure_win();
             }
             data::GameResult::Loss => {
-                typeln(&String::from("Loss!"), &cfg);
+                typeln(&format!("Loss! -({}{})", cfg.currency_prefix, bet), &cfg);
                 stats.pure_loss();
             }
             data::GameResult::Blackjack => {
-                typeln(&String::from("Blackjack!!!"), &cfg);
+                typeln(
+                    &format!("Blackjack!!! +({}{})", cfg.currency_prefix, bet),
+                    &cfg,
+                );
                 stats.increase_wallet(bet * 2);
                 stats.blackjack();
             }
             data::GameResult::Bust => {
-                typeln(&String::from("Bust!"), &cfg);
+                typeln(&format!("Bust! -({}{})", cfg.currency_prefix, bet), &cfg);
                 stats.bust();
             }
             data::GameResult::Push => {
@@ -171,7 +215,10 @@ fn main() {
                 stats.push();
             }
             data::GameResult::DealerBust => {
-                typeln(&String::from("Dealer bust!"), &cfg);
+                typeln(
+                    &format!("Dealer bust! +({}{})", cfg.currency_prefix, bet),
+                    &cfg,
+                );
                 stats.increase_wallet(bet * 2);
                 stats.dealer_bust();
             }
@@ -240,6 +287,23 @@ fn hand_value(hand: &deckofcards::Hand) -> u8 {
     value
 }
 
+fn card_value(card: &Card) -> u8 {
+    match card.rank {
+        deckofcards::Rank::Two => 2,
+        deckofcards::Rank::Three => 3,
+        deckofcards::Rank::Four => 4,
+        deckofcards::Rank::Five => 5,
+        deckofcards::Rank::Six => 6,
+        deckofcards::Rank::Seven => 7,
+        deckofcards::Rank::Eight => 8,
+        deckofcards::Rank::Nine => 9,
+        deckofcards::Rank::Ten => 10,
+        deckofcards::Rank::Jack => 10,
+        deckofcards::Rank::Queen => 10,
+        deckofcards::Rank::King => 10,
+        deckofcards::Rank::Ace => 11,
+    }
+}
 fn quit(config: &data::Configuration, stats: &data::Statistics) {
     // Print statistics
     typeln(
